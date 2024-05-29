374ed5c4184c8901276c234f72d66380570ab35e
programermakoto <makoto17720620@gmail.com>
2024-05-29 12:11:25 +0900

    php update

---
+++ quiz/PHP_quiz/quiz-1.js
@@ -0,0 +1,64 @@
+const questions = [
+  { question: "HTMLとは何の略？", choices: ["Hyperlink and Text Markup Language", "Hyper Tool Markup Language", "High Text Markup Language", "Hyper Text Markup Language", "High Transfer Markup Language"], answer: "Hyper Text Markup Language" },
+];
+let currentQuestionIndex = 0;
+let correctCount = 0;
+
+function displayQuestion(questionIndex) {
+  const questionObj = questions[questionIndex];
+  const questionElement = document.getElementById('question');
+  questionElement.textContent = questionObj.question;
+
+  const choicesElement = document.getElementById('choices');
+  choicesElement.innerHTML = '';
+
+  questionObj.choices.forEach((choice, index) => {
+      const li = document.createElement('li');
+      const button = document.createElement('button');
+      button.textContent = choice;
+      button.onclick = () => checkAnswer(choice, questionObj.answer);
+      li.appendChild(button);
+      choicesElement.appendChild(li);
+  });
+}
+
+function checkAnswer(selected, correct) {
+  const feedbackElement = document.getElementById('feedback');
+  if (selected === correct) {
+      feedbackElement.textContent = "正解です!!!!!!!!　おめでとうございます！";
+      correctCount++;
+  } else {
+      feedbackElement.textContent = `不正解! 答えは ${correct} です。`;
+  }
+
+  if (currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++;
+  } else {
+      showResult();
+      return;
+  }
+
+  setTimeout(() => {
+      feedbackElement.textContent = '';
+      displayQuestion(currentQuestionIndex);
+  }, 2000);
+}
+
+function nextQuestion() {
+  const feedbackElement = document.getElementById('feedback');
+  feedbackElement.textContent = '';
+
+  if (currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++;
+      displayQuestion(currentQuestionIndex);
+  } else {
+      showResult();
+  }
+}
+
+function showResult() {
+  const quizContainer = document.getElementById('quiz-container');
+  quizContainer.innerHTML = `<h1>結果発表</h1><p>${questions.length}問中<span>${correctCount}問</span>正解!!</p><br><a class="quiz-r-btn" href="quiz-1.html">もう一度する</a>`;
+}
+
+displayQuestion(0);
